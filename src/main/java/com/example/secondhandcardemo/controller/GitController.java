f50182e74b40382a2ea021bbfce6e797201ef344
Aleo <2923509522@qq.com>
2023-10-28 19:30:17 +0800

    test2

--- src/main/java/com/example/secondhandcardemo/controller/GitController.java
+++ src/main/java/com/example/secondhandcardemo/controller/GitController.java
@@ -1,8 +1,25 @@
 package com.example.secondhandcardemo.controller;
 
+import com.example.secondhandcardemo.pojo.Car;
+import com.example.secondhandcardemo.pojo.Response;
+import com.example.secondhandcardemo.pojo.response.Code;
+import com.example.secondhandcardemo.pojo.response.Msg;
+import com.example.secondhandcardemo.service.serviceImpl.AddSaleServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GitController {
     //Test1
+    //Test2
+    @Autowired
+    private AddSaleServiceImpl addSaleService;
+
+    @PostMapping("/sale/add")
+    public Response addSaler(@RequestBody Car car) {
+        addSaleService.addNewSaler(car);
+        return new Response(Code.RUN_SUCCESS, Msg.RUN_SUCCESS,"hello world add saler");
+    }
 }
